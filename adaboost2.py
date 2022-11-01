d3df605ed3244324d8afa8adfe271005cb1c4389
Ayesh Buddhima <ayeshbuddima@gmail.com>
2022-11-01 14:42:20 -0700

    V6

--- adaboost2.py
+++ adaboost2.py
@@ -22,13 +22,23 @@ def compute_error(y, y_pred, w):
 
 def compute_alpha(error):
     #alpha = np.log((1 - error) / error)
-    alpha = 0.5 * np.log((1 - error) / error)
+    alpha = 0.5 * np.log2((1 - error) / error)
     return alpha
 
 #update weights after an iteration
 def update_weights(w, alpha, y, y_pred):
     #new_weights = w * np.exp(alpha * (np.not_equal(y, y_pred)).astype(int))
-    new_weights = w * np.exp(alpha * (y * y_pred).astype(int))
+    new_weights = w * np.exp(-1* alpha * (y * y_pred).astype(int))
+    # print("new",new_weights)
+    sum = 0
+    for i in range(len(new_weights)):
+        sum += new_weights[i]
+    # print("sum",sum)
+    z = 1/sum
+    # print("z", z)
+    for i in range(len(new_weights)):
+        new_weights[i] = new_weights[i]*z
+    # print("new2", new_weights)
     return new_weights
 
 
@@ -40,24 +50,26 @@ class AdaBoost():
         self.alpha_list = []
         self.clf_list = []
         self.max_iter = None
-        self.training_errors = []
+        self.error_list = []
         self.prediction_errors = []
 
     def fit(self, X, y, max_iter):
         # Clear before calling
         self.alpha_list = []
-        self.training_errors = []
+        self.error_list = []
         self.max_iter = max_iter
 
         # Iterate over max_iter weak classifiers
-        for m in range(0, max_iter):
+        for i in range(0, max_iter):
 
             # Set weights for current boosting iteration
-            if m == 0:
+            if i == 0:
                 N = len(y)    #Num.of Samples
                 w = np.ones(N) * 1 / N  # At m = 0, weights are all the same and equal to 1 / N
+                print("w",w)
             else:
                 w = update_weights(w, alpha, y, y_pred)
+                print("w", w)
             # print(w)
 
             # (a) Fit weak classifier and predict labels
@@ -69,11 +81,13 @@ class AdaBoost():
 
             # (b) Compute error
             error = compute_error(y, y_pred, w)
-            self.training_errors.append(error)
+            print("error",error)
+            self.error_list.append(error)
             # print(error_m)
 
             # (c) Compute alpha
             alpha = compute_alpha(error)
+            print("alpha",alpha)
             self.alpha_list.append(alpha)
             # print(alpha_m)
 
@@ -81,14 +95,19 @@ class AdaBoost():
 
     def predict(self, X):
         # Initialise dataframe with weak predictions for each observation
-        weak_preds = pd.DataFrame(index=range(len(X)), columns=range(self.max_iter))
-        print("weak_preds",weak_preds)
+        # weak_preds = pd.DataFrame(index=range(len(X)), columns=range(self.max_iter))
+        # print("weak_preds",weak_preds)
+        data_list =np.empty(shape=(self.max_iter,len(X)))
         # Predict class label for each weak classifier, weighted by alpha_m
         for i in range(self.max_iter):
             y_pred_i = self.clf_list[i].predict(X) * self.alpha_list[i]
-            print(y_pred_i)
-            weak_preds.iloc[:, i] = y_pred_i
-        print("weak_preds2", weak_preds)
+            data_list[i] = y_pred_i
+            # print(y_pred_i)
+            # weak_preds.iloc[:, i] = y_pred_i
+        weak_preds = data_list.transpose()
+        # print("list", data_list2)
+        # print("weak_preds2", weak_preds)
+
 
         # Estimate final predictions
         y_pred = (1 * np.sign(weak_preds.T.sum())).astype(int)
