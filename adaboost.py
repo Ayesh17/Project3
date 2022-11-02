20209e6048a3dd42693c79bef1e9d540705a56b7
Ayesh Buddhima <ayeshbuddima@gmail.com>
2022-11-01 17:02:20 -0700

    V7

--- adaboost.py
+++ adaboost.py
@@ -1,41 +1,147 @@
+# Imports
 import numpy as np
 from sklearn import tree
+import pandas as pd
 
 def adaboost_train(X,Y,max_iter):
-    ada =Adaboost
-    Adaboost.fit(X,Y,max_iter)
+    ada =AdaBoost()
+    ada.fit(X,Y,max_iter)
 
-    return 0, 0
+    alpha_list = ada.alpha_list
+    clf_list = ada.clf_list
+    print(ada.alpha_list)
+    print(ada.clf_list)
 
-def error(y, y_pred, w):
-    # error = sum(w*(np.not_equal(y,y_pred)).astype(int)) / sum(w)
+    return clf_list, alpha_list
+
+def adaboost_test(X,Y,f,alpha):
+    ada = AdaBoost(clf_list=f,alpha_list=alpha)
+    y=ada.predict(X, Y)
+    print("y",y)
+    return 0
+
+# Helper functions
+def compute_error(y, y_pred, w):
+    #error = (sum(w * (np.not_equal(y, y_pred)).astype(int))) / sum(w)
     error = sum(w * (np.not_equal(y, y_pred)).astype(int))
     return error
 
-def alpha(error):
-    alpha = np.log((1 - error) / error)
+
+def compute_alpha(error):
+    #alpha = np.log((1 - error) / error)
+    alpha = 0.5 * np.log2((1 - error) / error)
     return alpha
 
-def update_weights(w,alpha,y,y_pred):
-    # new_weight = w * np.exp(alpha * (np.not_equal(y, y_pred)).astype(int))
-    new_weight = w * np.exp(alpha) * y * y_pred
-    return new_weight
+#update weights after an iteration
+def update_weights(w, alpha, y, y_pred):
+    #new_weights = w * np.exp(alpha * (np.not_equal(y, y_pred)).astype(int))
+    new_weights = w * np.exp(-1* alpha * (y * y_pred).astype(int))
+    # print("new",new_weights)
+    sum = 0
+    for i in range(len(new_weights)):
+        sum += new_weights[i]
+    # print("sum",sum)
+    z = 1/sum
+    # print("z", z)
+    for i in range(len(new_weights)):
+        new_weights[i] = new_weights[i]*z
+    # print("new2", new_weights)
+    return new_weights
 
-class Adaboost:
-    def __int__(self):
-        # self.alphas = []
-        # self.G_M = []
-         self.M = None
-        # self.training_errors = []
+
+# Define AdaBoost class
+class AdaBoost():
+
+    def __init__(self,alpha_list = [],clf_list = []):
+        # self.w = None
+        self.alpha_list = alpha_list
+        self.clf_list = clf_list
+        self.max_iter = None
+        # self.error_list = []
         # self.prediction_errors = []
 
-    def fit(X, Y, max_iter):
-        # initialize weights to 1/N
-        N = len(X)
-        w = np.full(N, (1 / N))
+    def fit(self, X, y, max_iter):
+        # Clear before calling
+        self.alpha_list = []
+        # self.error_list = []
+        self.max_iter = max_iter
 
-        for i in range(max_iter):
-            clf = tree.DecisionTreeClassifier(max_depth=1)
-            clf = clf.fit(X,Y)
-            f = clf.predict(X,Y)
-            print(f)
+        # Iterate over max_iter weak classifiers
+        for i in range(0, max_iter):
+
+            # Set weights for current boosting iteration
+            if i == 0:
+                N = len(y)    #Num.of Samples
+                w = np.ones(N) * 1 / N  # At m = 0, weights are all the same and equal to 1 / N
+                # print("w",w)
+            else:
+                w = update_weights(w, alpha, y, y_pred)
+                # print("w", w)
+            # print(w)
+
+            # (a) Fit weak classifier and predict labels
+            clf = tree.DecisionTreeClassifier(max_depth=1)  # Stump: Two terminal-node classification tree
+            clf.fit(X, y, sample_weight=w)
+            y_pred = clf.predict(X)
+            print("y",y_pred)
+
+            self.clf_list.append(clf)  # Save to list of weak classifiers
+
+            # (b) Compute error
+            error = compute_error(y, y_pred, w)
+            # print("error",error)
+            # self.error_list.append(error)
+            # print(error_m)
+
+            # (c) Compute alpha
+            alpha = compute_alpha(error)
+            # print("alpha",alpha)
+            self.alpha_list.append(alpha)
+            # print(alpha_m)
+
+        assert len(self.clf_list) == len(self.alpha_list)
+
+    def predict(self, X, Y):
+        # Initialise dataframe with weak predictions for each observation
+        self.max_iter = 5
+        weak_preds = pd.DataFrame(index=range(len(X)), columns=range(self.max_iter))
+        print("weak_preds",weak_preds)
+        print("X",len(self.alpha_list))
+
+        # weak_preds =np.empty(shape=(len(self.clf_list),len(X)))
+        # Predict class label for each weak classifier, weighted by alpha_m
+        preds =[]
+        for i in range(len(self.clf_list)):
+            # print("clf",self.clf_list[i].predict(X))
+            # print("alpha", self.alpha_list[i])
+            # y_pred_i = self.clf_list[i].predict(X)
+            # print("y_pred_i",y_pred_i)
+            # preds.append(y_pred_i)
+
+            y_pred_ialpha = self.alpha_list[i] * self.clf_list[i].predict(X)
+            # weak_preds[i] = y_pred_ialpha
+            # print("y_pred_i",y_pred_i)
+            weak_preds.iloc[:, i] = y_pred_ialpha
+        # weak_preds = weak_preds
+        # print("list", data_list2)
+        # print("weak_preds2", weak_preds)
+        # print("preds", preds)
+        #
+        #
+        # # Estimate final predictions
+        # print("sum",weak_preds[0].T.sum())
+        y_pred = (1 * np.sign(weak_preds.T.sum())).astype(int)
+        print("y_pred",y_pred)
+        # print("Y", Y)
+
+        return y_pred
+
+    # def error_rates(self, X, y):
+    #
+    #     self.prediction_errors = []  # Clear before calling
+    #
+    #     # Predict class label for each weak classifier
+    #     for i in range(self.max_iter):
+    #         y_pred = self.clf_list[i].predict(X)
+    #         error = compute_error(y=y, y_pred=y_pred, w=np.ones(len(y)))
+    #         self.prediction_errors.append(error)
--- adaboost2.py
+++ adaboost2.py
@@ -1,18 +1,23 @@
-import numpy as np
-from sklearn import tree
 # Imports
 import numpy as np
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 
 def adaboost_train(X,Y,max_iter):
     ada =AdaBoost()
-    x=ada.fit(X,Y,max_iter)
-    y=ada.predict(X)
+    ada.fit(X,Y,max_iter)
+
+    alpha_list = ada.alpha_list
+    clf_list = ada.clf_list
     print(ada.alpha_list)
     print(ada.clf_list)
 
-    return 0, 0
+    return clf_list, alpha_list
+
+def adaboost_test(X,Y,f,alpha):
+    ada = AdaBoost(clf_list=f,alpha_list=alpha)
+    acc =ada.predict(X, Y)
+    return acc
+
 # Helper functions
 def compute_error(y, y_pred, w):
     #error = (sum(w * (np.not_equal(y, y_pred)).astype(int))) / sum(w)
@@ -45,18 +50,18 @@ def update_weights(w, alpha, y, y_pred):
 # Define AdaBoost class
 class AdaBoost():
 
-    def __init__(self):
+    def __init__(self,alpha_list = [],clf_list = []):
         # self.w = None
-        self.alpha_list = []
-        self.clf_list = []
+        self.alpha_list = alpha_list
+        self.clf_list = clf_list
         self.max_iter = None
-        self.error_list = []
-        self.prediction_errors = []
+        # self.error_list = []
+        # self.prediction_errors = []
 
     def fit(self, X, y, max_iter):
         # Clear before calling
         self.alpha_list = []
-        self.error_list = []
+        # self.error_list = []
         self.max_iter = max_iter
 
         # Iterate over max_iter weak classifiers
@@ -66,60 +71,81 @@ class AdaBoost():
             if i == 0:
                 N = len(y)    #Num.of Samples
                 w = np.ones(N) * 1 / N  # At m = 0, weights are all the same and equal to 1 / N
-                print("w",w)
+                # print("w",w)
             else:
                 w = update_weights(w, alpha, y, y_pred)
-                print("w", w)
+                # print("w", w)
             # print(w)
 
             # (a) Fit weak classifier and predict labels
-            clf = DecisionTreeClassifier(max_depth=1)  # Stump: Two terminal-node classification tree
+            clf = tree.DecisionTreeClassifier(max_depth=1)  # Stump: Two terminal-node classification tree
             clf.fit(X, y, sample_weight=w)
             y_pred = clf.predict(X)
+            print("y",y_pred)
 
             self.clf_list.append(clf)  # Save to list of weak classifiers
 
             # (b) Compute error
             error = compute_error(y, y_pred, w)
-            print("error",error)
-            self.error_list.append(error)
+            # print("error",error)
+            # self.error_list.append(error)
             # print(error_m)
 
             # (c) Compute alpha
             alpha = compute_alpha(error)
-            print("alpha",alpha)
+            # print("alpha",alpha)
             self.alpha_list.append(alpha)
             # print(alpha_m)
 
-        assert len(self.clf_list) == len(self.alpha_list)
+        # assert len(self.clf_list) == len(self.alpha_list)
 
-    def predict(self, X):
+    def predict(self, X, Y):
         # Initialise dataframe with weak predictions for each observation
         # weak_preds = pd.DataFrame(index=range(len(X)), columns=range(self.max_iter))
         # print("weak_preds",weak_preds)
-        data_list =np.empty(shape=(self.max_iter,len(X)))
+        # print("X",len(self.alpha_list))
+
+        weak_preds =np.empty(shape=(len(self.clf_list),len(X)))
         # Predict class label for each weak classifier, weighted by alpha_m
-        for i in range(self.max_iter):
-            y_pred_i = self.clf_list[i].predict(X) * self.alpha_list[i]
-            data_list[i] = y_pred_i
-            # print(y_pred_i)
-            # weak_preds.iloc[:, i] = y_pred_i
-        weak_preds = data_list.transpose()
-        # print("list", data_list2)
-        # print("weak_preds2", weak_preds)
 
+        # get predictions by classifier list
+        for i in range(len(self.clf_list)):
+            y_pred = self.alpha_list[i] * self.clf_list[i].predict(X)
+            weak_preds[i] = y_pred
 
-        # Estimate final predictions
-        y_pred = (1 * np.sign(weak_preds.T.sum())).astype(int)
+        weak_preds = weak_preds.transpose()
 
-        return y_pred
+        #get signs
+        weak_preds_signs = np.sign(weak_preds)
 
-    def error_rates(self, X, y):
+        #get final prediction by vote
+        preds=[]
+        for i in range(len(weak_preds_signs)):
+            sum = 0
+            for j in range(len(weak_preds_signs[i])):
+                sum += weak_preds_signs[i][j]
+            preds.append(sum)
+        preds = np.sign(preds)
 
-        self.prediction_errors = []  # Clear before calling
+        #get accuracy
+        correct = 0
+        wrong = 0
+        for i in range(len(preds)):
+            if (preds[i] == Y[i]):
+                correct += 1
+            else:
+                wrong += 1
 
-        # Predict class label for each weak classifier
-        for i in range(self.max_iter):
-            y_pred = self.clf_list[i].predict(X)
-            error = compute_error(y=y, y_pred=y_pred, w=np.ones(len(y)))
-            self.prediction_errors.append(error)
+        accuracy = correct / (correct + wrong)
+
+        return accuracy
+
+    # def error_rates(self, X, y):
+    #
+    #     self.prediction_errors = []  # Clear before calling
+    #
+    #     # Predict class label for each weak classifier
+    #     for i in range(self.max_iter):
+    #         y_pred = self.clf_list[i].predict(X)
+    #         error = compute_error(y=y, y_pred=y_pred, w=np.ones(len(y)))
+    #         self.prediction_errors.append(error)
